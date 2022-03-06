719f625ad758f7658eab98a609d16f319c0406d8
shoaib <mshoaib.85@gmail.com>
2022-03-06 22:06:51 +0500

    replaced obselete selenium functions

--- booking/booking.py
+++ booking/booking.py
@@ -1,6 +1,7 @@
 import booking.constants as const
 import os
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 
 class Booking(webdriver.Chrome):
@@ -21,22 +22,17 @@ class Booking(webdriver.Chrome):
         self.get(const.BASE_URL)
 
     def change_currency(self, currency=None):
-        currency_element = self.find_element_by_css_selector(
-            'button[data-tooltip-text="Choose your currency"]'
-        )
+        currency_element = self.find_elements(by=By.CSS_SELECTOR,
+                                               value='button[data-tooltip-text="Choose your currency"]')[0]
         currency_element.click()
-        selected_currency_element = self.find_element_by_css_selector(
-            f'a[data-modal-header-async-url-param*="selected_currency={currency}"]'
-        )
+        selected_currency_element = self.find_elements(by=By.CSS_SELECTOR,
+                                                       value=f'a[data-modal-header-async-url-param*="selected_currency={currency}"]')[0]
         selected_currency_element.click()
 
-
     def select_place_to_go(self, place_to_go):
-        search_field = self.find_element_by_id('ss')
+        search_field = self.find_elements(By.ID, value='ss')[0]
         search_field.clear()
         search_field.send_keys(place_to_go)
 
-        first_result = self.find_element_by_css_selector(
-            'li[data-i="0"]'
-        )
+        first_result = self.find_elements(By.CSS_SELECTOR, value='li[data-i="0"]')[0]
         first_result.click()
