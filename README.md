9560d8301438a853354abd961dfea7a3657f6bab
shoaib <mshoaib.85@gmail.com>
2022-03-06 23:10:48 +0500

    python code is updated

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-#Selenium Bot for BBC Urdu
+#Selenium Bot for Booking.com
 
 ##  Prerequisites:
 - Python Installed (Recommended version 3.8 or above)
@@ -6,14 +6,14 @@
     - Install pipenv (use `pip install pipenv`)
     - Create virtual environment in current directory (use `pipenv install`)
     - Install latest selenium version (use `pipenv install selenium`)
+    - Install prettytable (use `pipenv install prettytable`)
     - Install PyQT5 for UI (use `pip install PyQt5`)
     - Install PyChart for visualisation (use `pip install PyQtChart`)
 
 ##  Goals:
-- Visit bbc seb site and download
-- Visit various sections
-- Grab interesting parts like
-  - World News 
-  - Sports
-  - Misc
+- Visit booking.com 
 - Use PyQT5 to create UI and show crawling results
+
+
+### Credits
+Thanks to [JimSC](https://github.com/jimdevops19/SeleniumSeries)
--- booking/booking.py
+++ booking/booking.py
@@ -1,16 +1,19 @@
 import booking.constants as const
 import os
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-
+from booking.booking_filtration import BookingFiltration
+from booking.booking_report import BookingReport
+from prettytable import PrettyTable
 
 class Booking(webdriver.Chrome):
-    def __init__(self, driver_path=r"C:\SeleniumDrivers",
+    def __init__(self, driver_path=r"F:\Softwares\Development\Pythons\Selenium",
                  teardown=False):
         self.driver_path = driver_path
         self.teardown = teardown
         os.environ['PATH'] += self.driver_path
-        super(Booking, self).__init__()
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option('excludeSwitches', ['enable-logging'])
+        super(Booking, self).__init__(options=options)
         self.implicitly_wait(15)
         self.maximize_window()
 
@@ -22,17 +25,84 @@ class Booking(webdriver.Chrome):
         self.get(const.BASE_URL)
 
     def change_currency(self, currency=None):
-        currency_element = self.find_elements(by=By.CSS_SELECTOR,
-                                               value='button[data-tooltip-text="Choose your currency"]')[0]
+        currency_element = self.find_element_by_css_selector(
+            'button[data-tooltip-text="Choose your currency"]'
+        )
         currency_element.click()
-        selected_currency_element = self.find_elements(by=By.CSS_SELECTOR,
-                                                       value=f'a[data-modal-header-async-url-param*="selected_currency={currency}"]')[0]
+
+        selected_currency_element = self.find_element_by_css_selector(
+            f'a[data-modal-header-async-url-param*="selected_currency={currency}"]'
+        )
         selected_currency_element.click()
 
+
     def select_place_to_go(self, place_to_go):
-        search_field = self.find_elements(By.ID, value='ss')[0]
+        search_field = self.find_element_by_id('ss')
         search_field.clear()
         search_field.send_keys(place_to_go)
 
-        first_result = self.find_elements(By.CSS_SELECTOR, value='li[data-i="0"]')[0]
+        first_result = self.find_element_by_css_selector(
+            'li[data-i="0"]'
+        )
         first_result.click()
+
+    def select_dates(self, check_in_date, check_out_date):
+        check_in_element = self.find_element_by_css_selector(
+            f'td[data-date="{check_in_date}"]'
+        )
+        check_in_element.click()
+
+        check_out_element = self.find_element_by_css_selector(
+            f'td[data-date="{check_out_date}"]'
+        )
+        check_out_element.click()
+
+    def select_adults(self, count=1):
+        selection_element = self.find_element_by_id('xp__guests__toggle')
+        selection_element.click()
+
+        while True:
+            decrease_adults_element = self.find_element_by_css_selector(
+                'button[aria-label="Decrease number of Adults"]'
+            )
+            decrease_adults_element.click()
+            #If the value of adults reaches 1, then we should get out
+            #of the while loop
+            adults_value_element = self.find_element_by_id('group_adults')
+            adults_value = adults_value_element.get_attribute(
+                'value'
+            ) # Should give back the adults count
+
+            if int(adults_value) == 1:
+                break
+
+        increase_button_element = self.find_element_by_css_selector(
+            'button[aria-label="Increase number of Adults"]'
+        )
+
+        for _ in range(count - 1):
+            increase_button_element.click()
+
+    def click_search(self):
+        search_button = self.find_element_by_css_selector(
+            'button[type="submit"]'
+        )
+        search_button.click()
+
+    def apply_filtrations(self):
+        filtration = BookingFiltration(driver=self)
+        filtration.apply_star_rating(4, 5)
+
+        filtration.sort_price_lowest_first()
+
+    def report_results(self):
+        hotel_boxes = self.find_element_by_id(
+            'hotellist_inner'
+        )
+
+        report = BookingReport(hotel_boxes)
+        table = PrettyTable(
+            field_names=["Hotel Name", "Hotel Price", "Hotel Score"]
+        )
+        table.add_rows(report.pull_deal_box_attributes())
+        print(table)
